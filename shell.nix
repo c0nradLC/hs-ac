f13f379635fd31c0ebe0ad8b51575684d5603264
c0nradLC <l.conrado10@gmail.com>
2025-12-19 09:47:13 -0300

    moved aimbot logic to GLHook and setup OpenGL manipulation

--- shell.nix
+++ shell.nix
@@ -1,9 +1,21 @@
 let
   pkgs = (import ./default.nix).pkgs;
 in
-pkgs.stdenv.mkDerivation {
-	name = "env";
-	buildInputs = [ pkgs.haskellPackages.haskell-language-server ]
-      ++ [ pkgs.haskellPackages.cabal-install]
-      ++ [ pkgs.mesa_glu pkgs.freeglut ];
+pkgs.haskellPackages.shellFor {
+  packages = p: [ 
+    (p.callCabal2nix "hs-ac" ./. {})
+  ];
+
+  buildInputs = [
+    pkgs.haskellPackages.cabal-install
+    pkgs.haskellPackages.haskell-language-server
+    pkgs.haskellPackages.OpenGL
+  ];
+
+  nativeBuildInputs = [
+    pkgs.mesa_glu
+    pkgs.libGL
+    pkgs.libGLU
+    pkgs.freeglut
+  ];
 }
--- src/GLHook.hs
+++ src/GLHook.hs
@@ -12,7 +12,7 @@ import Control.Monad
 import Data.IORef
 import System.IO
 import System.IO.Unsafe
-import Data.List (isInfixOf, find)
+import Data.List (isInfixOf, find, minimumBy)
 import Control.Concurrent
 import System.Posix.DynamicLinker
 import qualified Memory as Mem
@@ -20,6 +20,10 @@ import qualified Data.ByteString as BS
 import Data.Word
 import GHC.Int (Int32)
 import Numeric (readHex, showHex)
+import Data.Ord (comparing)
+import Data.Maybe (fromMaybe)
+import qualified Graphics.Rendering.OpenGL as GL
+import Graphics.Rendering.OpenGL (get)
 
 -- SDL2 Window type (opaque pointer)
 type SDL_Window = Ptr ()
@@ -40,6 +44,12 @@ sdlGLSwapWindowHook _ = do
             return $ Mem._baseAddr gameModule
         Nothing -> do
             error "Game/Binary module not found."
+
+    GL.matrixMode GL.$= GL.Projection
+    GL.loadIdentity
+
+    test <- get GL.viewport
+    print $ show test
 
     let playerEntityPointer = gameModuleBaseAddr + (fst . head $ readHex "19d518")
     let playerListPointer = gameModuleBaseAddr + (fst . head $ readHex "19d520")
@@ -71,11 +81,11 @@ sdlGLSwapWindowHook _ = do
         Just playerEntityAddr -> do 
             let healthAddr = playerEntityAddr + (fst . head $ readHex "100")
             let ammoAddr = playerEntityAddr + (fst . head $ readHex "154")
-            --let playerAimYAddr = playerEntityAddr + (fst . head $ readHex "3C")
             let playerPosAddr = playerEntityAddr + (fst . head $ readHex "8")
+            let playerAimYAddr = playerEntityAddr + (fst . head $ readHex "3C")
+            let playerAimXAddr = playerEntityAddr + (fst . head $ readHex "38")
             
             mPlayerPos <- Mem.readVec3 pid playerPosAddr
-            print $ "Player position: " ++ show mPlayerPos
             
             -- Set primary ammo
             Mem.writeMem memPath ammoAddr 1337
@@ -88,12 +98,15 @@ sdlGLSwapWindowHook _ = do
             --print $ "HealthAddr val: " ++ show test2
 
             mMaxPlayers <- Mem.readInt32 pid maxPlayersAddress
+            mPlayerState <- Mem.readInt32 pid (playerEntityAddr + playerStateOffset)
+            mPlayerPos <- Mem.readVec3 pid (playerEntityAddr + playerPosOffset)
+            mPlayerTeam <- Mem.readInt32 pid (playerEntityAddr + playerTeamOffset)
 
-            playerState <- Mem.readInt32 pid (playerEntityAddr + playerStateOffset)
-            playerPos <- Mem.readVec3 pid (playerEntityAddr + playerPosOffset)
-            playerTeam <- Mem.readInt32 pid (playerEntityAddr + playerTeamOffset)
+            let playerState = fromMaybe 3 mPlayerState
+                playerPos = fromMaybe (0,0,0) mPlayerPos
+                playerTeam = fromMaybe 3 mPlayerTeam
 
-            let localPlayer = Player {_pos = playerPos, _state = playerState, _team = playerTeam}
+            let localPlayer = Player {_pos = playerPos, _state = playerState, _team = playerTeam, _distance = Nothing}
             case mMaxPlayers of
                 Just maxPlayers -> do
                     mPlayersListAddress <- Mem.readAddress pid playerListPointer
@@ -101,21 +114,39 @@ sdlGLSwapWindowHook _ = do
                         Just playersListAddr -> do
                             let firstBotAddress = playersListAddr + (fst . head $ readHex "8")
                             let sndBotAddress = playersListAddr + (fst . head $ readHex "10")
-                            let botPointersTest = getBotsPointers firstBotAddress sndBotAddress maxPlayers
+                            let botPointers = getBotsPointers firstBotAddress sndBotAddress maxPlayers
 
-                            mapM_ (\botPointer -> do
-                                    mBotAddress <- Mem.readAddress pid botPointer
-                                    case mBotAddress of 
-                                        Just botAddress -> do
-                                            botState <- Mem.readInt32 pid (botAddress + playerStateOffset)
-                                            botPos <- Mem.readVec3 pid (botAddress + playerPosOffset)
-                                            botTeam <- Mem.readInt32 pid (botAddress + playerTeamOffset)
-
-                                            let bot = Player {_pos = botPos, _state = botState, _team = botTeam}
-                                            print $ show bot
-                                            return ()
-                                        Nothing -> return ()
-                                    ) botPointersTest
+                            bots <- mapM (\botPointer -> do
+                                mBotAddress <- Mem.readAddress pid botPointer
+                                case mBotAddress of
+                                    Just botAddress -> do
+                                        mBotState <- Mem.readInt32 pid (botAddress + playerStateOffset)
+                                        mBotPos <- Mem.readVec3 pid (botAddress + playerPosOffset)
+                                        mBotTeam <- Mem.readInt32 pid (botAddress + playerTeamOffset)
+                                        let botState = fromMaybe 4 mBotState
+                                            botPos = fromMaybe (0,0,0) mBotPos
+                                            botTeam = fromMaybe 4 mBotTeam
+                                            (deltaX, deltaY, _) = getDeltas (_pos localPlayer) (_pos bot)
+                                            bot = Player
+                                                { _pos = botPos
+                                                , _state = botState
+                                                , _team = botTeam
+                                                , _distance = Just $ getDistance (deltaX, deltaY)
+                                                }
+                                        if _team bot /= _team localPlayer && _state bot == 0 then
+                                            return $ Just bot
+                                        else return Nothing
+                                    Nothing -> return Nothing
+                                ) botPointers
+                            let mTarget = getClosestBot bots
+                            case mTarget of
+                                Just target -> do
+                                    let (aimX, aimY) = getAngles (_pos localPlayer) (_pos target)
+                                    Mem.writeFloat memPath playerAimYAddr aimY
+                                    Mem.writeFloat memPath playerAimXAddr aimX
+                                    return ()
+                                Nothing -> return ()
+                            return ()
                         Nothing -> return ()
                 Nothing -> return ()
         Nothing -> return()
@@ -130,6 +161,31 @@ getBotsPointers :: Word64 -> Word64 -> Int32 -> [Word64]
 getBotsPointers fstAddr sndAddr maxPlayers =
     [fstAddr + fromIntegral i * nextBotOffset | i <- [0 .. maxPlayers-3]] ++
     [sndAddr + fromIntegral i * nextBotOffset | i <- [0 .. maxPlayers-4]]
+
+getAngles :: (Float, Float, Float) -> (Float, Float, Float) -> (Float, Float)
+getAngles playerPos botPos = do
+    let (deltaX, deltaY, deltaZ) = getDeltas playerPos botPos
+
+        yawRad = atan2 deltaX (-deltaY)
+        yawDeg = yawRad * 180.0 / pi
+        yawDeg' = if yawDeg < 0 then yawDeg + 360.0 else yawDeg
+
+        horizontalDistance = getDistance (deltaX, deltaY)
+
+        pitchRad = atan2 deltaZ horizontalDistance
+        pitchDeg = pitchRad * 180.0 / pi
+    (yawDeg', pitchDeg)
+
+getDistance :: (Float, Float) -> Float
+getDistance (deltaX, deltaY) = sqrt ((deltaX * deltaX) + (deltaY * deltaY))
+
+getDeltas :: (Float, Float, Float) -> (Float, Float, Float) -> (Float, Float, Float)
+getDeltas (playerX, playerY, playerZ) (botX, botY, botZ) = (botX - playerX, botY - playerY, botZ - playerZ)
+
+getClosestBot :: [Maybe Player] -> Maybe Player
+getClosestBot ms =
+  let candidates = [(p, d) | Just p <- ms, Just d <- [_distance p]]
+  in if null candidates then Nothing else Just (fst (minimumBy (comparing snd) candidates))
 
 nextBotOffset :: Word64
 nextBotOffset = fst . head $ readHex "10"
@@ -148,8 +204,9 @@ playerNameOffset = fst . head $ readHex "219"
 
 data Player
     = Player
-    { _pos   :: Maybe (Float, Float, Float)
-    , _team  :: Maybe Int32
-    , _state :: Maybe Int32
+    { _pos      :: (Float, Float, Float)
+    , _team     :: Int32
+    , _state    :: Int32
+    , _distance :: Maybe Float
     }
     deriving Show
