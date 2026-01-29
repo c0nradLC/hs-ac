3580307e261e8fae2ea4aa2fb2c82edd38414b6b
Leonardo Conrado <l.conrado10@gmail.com>
2026-01-28 22:03:48 -0300

    updated cabal file and build/run scripts

--- build.sh
+++ build.sh
@@ -1,6 +1,2 @@
-haskell_lib_name=libachook.so
-cpp_lib_name=libac_hook.so
-
-mkdir -p $PWD/out &&
-cabal build --ghc-options="-shared -fPIC -dynamic -lHSrts-ghc9.2.8 -ldl -o $PWD/out/$haskell_lib_name -no-hs-main" &&
-gcc $PWD/out/$haskell_lib_name -shared -fPIC $PWD/src/wrapper.c -o $PWD/out/$cpp_lib_name -ldl
+cabal build --enable-shared &&
+gcc hs-ac.so -shared -fPIC ./src/wrapper.c -o libachook.so -ldl
--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PWD/out &&
-LD_PRELOAD=$PWD/out/libac_hook.so $1
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PWD &&
+LD_PRELOAD=$PWD/libachook.so $1
