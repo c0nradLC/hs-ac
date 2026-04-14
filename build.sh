c34b1c3330f3c98733d06484a6e06752628b1014
Leonardo Conrado <l.conrado10@gmail.com>
2026-04-14 20:44:43 -0300

    Removed nix, using cabal only for now

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 cabal build --enable-shared &&
-gcc hs-ac.so -shared -fPIC ./src/wrapper.c -o libachook.so -ldl
+cp ./dist-newstyle/build/x86_64-linux/ghc-9.8.4/hs-ac-0.1.0.0/f/hs-ac/build/hs-ac/libhs-ac.so.0.0.0 ./libhs-ac.so.0 &&
+gcc ./libhs-ac.so.0 -shared -fPIC ./src/wrapper.c -o libachook.so -ldl
